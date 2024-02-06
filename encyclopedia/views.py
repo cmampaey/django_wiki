27c428ff0703b5abdd693704d66ca87659fc9dbb
Cédric <cedricmampaey@gmail.com>
2024-02-06 22:48:50 +0100

    add new page, 1:27:00 of lecture

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django import forms
+
 
 from . import util
 
+class NewEntryForm (forms.Form):
+    title = forms.CharField(label="Title")
+    body = forms.CharField(label="Body")
+    
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -9,4 +15,17 @@ def index(request):
     })
 
 def new(request):
-    return render(request, "encyclopedia/new.html")
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data[entry]
+            entries.append(entry)
+        else:
+            return render(request, "encyclopedia/new.html", {
+        "form": form
+         })
+            
+    return render(request, "encyclopedia/new.html", {
+        "form": NewEntryForm()
+    })
+        
